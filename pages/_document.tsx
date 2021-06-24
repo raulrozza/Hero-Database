4fde9221d7cf9a79db940a0166a37e6581c70fa4
Raul <raul.hmetal@gmail.com>
2021-06-24 11:20:51 -0300

    Edit: Finish adding theme config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ class MyDocument extends Document {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" />
         </Head>
 
         <body>
---
+++ src/config/theme/index.ts
@@ -0,0 +1,11 @@
+import layout from './layout';
+import {lightPalette, darkPalette} from './palette';
+import typography from './typography';
+
+const buildTheme = (mode: 'light' | 'dark') => ({
+  layout,
+  palette: mode === 'light' ? lightPalette : darkPalette,
+  typography,
+});
+
+export default buildTheme;
