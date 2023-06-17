a204884f5f4ce5bbc57d1ba499d472cfe6321f99
Raul Mendes Rosá <raul.hmetal@gmail.com>
2023-06-16 22:13:14 -0300

    feat: configuring conditions repository

--- src/modules/conditions/infra/db/conditions.ts
+++ src/modules/conditions/infra/db/conditions.ts
@@ -1,9 +1,19 @@
-import { ICondition } from '@/shared/domain/entities';
-
-type MetaCondition = {
-  key: string;
+export type MetaCondition = {
   name: string;
   description: string;
 } & ({ type: 'basic' } | { type: 'combined'; components: string[] });
 
-export const conditions = {};
+export const conditions = {
+  Compelled: {
+    type: 'basic',
+    name: 'Compelled',
+    description:
+      'A compelled character is directed by an outside force, but struggling against it; the character is limited to free actions and a single standard action per turn, chosen by another, controlling, character. As usual, this standard action can be traded for a move or even free action. Controlled supersedes compelled.',
+  } as MetaCondition,
+  Controlled: {
+    type: 'basic',
+    name: 'Controlled',
+    description:
+      'A controlled character has no free will; the character’s actions each turn are dictated by another, controlling, character.',
+  } as MetaCondition,
+};
--- src/modules/conditions/infra/repositories/implementations/ConditionsRepository.ts
+++ src/modules/conditions/infra/repositories/implementations/ConditionsRepository.ts
@@ -1,9 +1,33 @@
 import { IConditionsRepository } from '@/modules/conditions/domain/repositories/IConditionsRepository';
-import { conditions } from '@/modules/conditions/infra/db';
+import { MetaCondition, conditions } from '@/modules/conditions/infra/db';
 import { ICondition } from '@/shared/domain/entities';
 
 export default class ConditionsRepository implements IConditionsRepository {
   public async findAll(): Promise<ICondition[]> {
-    return conditions;
+    return Object.entries(conditions).map(([key, condition]) =>
+      this.convertCondition(key, condition),
+    );
+  }
+
+  private convertCondition(key: string, condition: MetaCondition): ICondition {
+    if (condition.type === 'basic')
+      return {
+        key,
+        name: condition.name,
+        description: condition.description,
+        type: condition.type,
+      };
+
+    return {
+      key,
+      name: condition.name,
+      description: condition.description,
+      type: condition.type,
+      components: condition.components.map(key => {
+        const data = conditions[key as keyof typeof conditions];
+
+        return this.convertCondition(key, data);
+      }),
+    };
   }
 }
