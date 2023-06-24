08b001fca0cca51933d57ed37445ed49c04bc51c
Raul Mendes Rosá <raul.hmetal@gmail.com>
2023-06-24 19:02:47 -0300

    feat: improving loading popover style

--- src/modules/conditions/presentation/components/organisms/ConditionPopoverContainer/styles.ts
+++ src/modules/conditions/presentation/components/organisms/ConditionPopoverContainer/styles.ts
@@ -4,5 +4,11 @@ export const Container = styled.div`
   ${({ theme }) => css`
     width: 100%;
     max-width: ${theme.layout.spacing(50)};
+    background-color: ${theme.palette.neutral[50]};
+
+    > span {
+      padding: ${theme.layout.spacing(4, 6)};
+      color: ${theme.palette.primary[800]};
+    }
   `}
 `;
