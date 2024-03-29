61e62db50abaf1e02dfae66f109486d39dc2876c
Raul Mendes Rosá <raul.hmetal@gmail.com>
2024-03-29 08:46:50 -0300

    feat: added all the extras

--- src/modules/powers/infra/db/modifiers.ts
+++ src/modules/powers/infra/db/modifiers.ts
@@ -573,4 +573,138 @@ export const modifiers = new Map<string, MetaModifier>([
       source: 'HH',
     },
   ],
+  [
+    'Secondary Effect',
+    {
+      name: 'Secondary Effect',
+      type: 'extra',
+      description: [
+        'An instant duration effect with this modifier affects the target once immediately (when the effect is used) and then once again on the following round, at the end of the attacker’s turn. The target gets the normal resistance check against the secondary effect.',
+        'Secondary Effects don’t stack, so if you attack a target with your Secondary Effect on the round after a successful hit, it doesn’t affect the target twice; it simply delays the second effect for another round. You can attack the target with a different effect, however. So, for example, if you hit a target with a Secondary @{Effect|Damage|Damage Effect} then, on the following round, hit with an @{Effect|Affliction}, the target suffers both the Affliction and the Secondary Damage.',
+      ],
+      cost: {
+        costs: [1],
+        maxRanks: -1,
+        type: 'standard',
+      },
+      source: 'HH',
+    },
+  ],
+  [
+    'Selective',
+    {
+      name: 'Selective',
+      type: 'extra',
+      description: [
+        'A resistible effect with this extra is discriminating, allowing you to decide who is and is not affected by it. This is most useful for @{Modifier|Area|area effects}. You must be able to accurately perceive a target in order to decide whether or not to affect it. For a degree of selectivity with non-resistible effects, use the @{Modifier|Precise} modifier.',
+      ],
+      cost: {
+        costs: [1],
+        maxRanks: -1,
+        type: 'standard',
+      },
+      source: 'HH',
+    },
+  ],
+  [
+    'Sleep',
+    {
+      name: 'Sleep',
+      type: 'extra',
+      description: [
+        'When this modifier is applied to an effect that causes the @{Condition|Incapacitated|incapacitated} condition, the effect leaves them @{Condition|Asleep|asleep} whenever it would normally render them incapacitated.',
+      ],
+      cost: {
+        costs: [0],
+        maxRanks: -1,
+        type: 'standard',
+      },
+      source: 'HH',
+    },
+  ],
+  [
+    'Split',
+    {
+      name: 'Split',
+      type: 'extra',
+      description: [
+        'With this modifier, a resistible effect that works on one target can split between two. The attacker chooses how many ranks to apply to each target up to the effect’s total rank. So a rank 10 effect could be split 5/5, 4/6, 2/8, or any other total adding up to 10. If an attack check is required, the attacker makes one, comparing the results against each target. The effect works on each target at its reduced rank.',
+        'Each additional rank of this modifier allows the power to split an additional time, so rank 2 allows an effect to split among three targets, then four, and so forth. An effect cannot split to less than 1 rank per target, and cannot apply more than one split to the same target. Thus maximum Split rank equals the effect’s rank.',
+      ],
+      cost: {
+        costs: [1],
+        maxRanks: -1,
+        type: 'flat',
+      },
+      source: 'HH',
+    },
+  ],
+  [
+    'Subtle',
+    {
+      name: 'Subtle',
+      type: 'extra',
+      description: [
+        'Subtle effects are not as noticeable. A subtle effect may be used to catch a target unaware and may in some cases qualify for a surprise attack. Rank 1 makes an effect difficult to notice; a DC 20 @{Skill|Perception} check is required, or the effect is noticeable only to certain exotic senses (at the GM’s discretion). Rank 2 makes the effect completely undetectable.',
+      ],
+      cost: {
+        costs: [1],
+        maxRanks: 2,
+        type: 'flat',
+      },
+      source: 'HH',
+    },
+  ],
+  [
+    'Sustained',
+    {
+      name: 'Sustained',
+      type: 'extra',
+      description: [
+        'Applied to a permanent duration effect, this modifier makes it sustained duration, requiring a free action to use (rather than none, like other permanent effects). The benefit is the sustained effect can be improved using extra effort, including using it to perform power stunts. The drawback is the effect requires a free action each turn to maintain it, and being unable to do so means the effect shuts off.',
+      ],
+      cost: {
+        costs: [0],
+        maxRanks: -1,
+        type: 'standard',
+      },
+      source: 'HH',
+    },
+  ],
+  [
+    'Triggered',
+    {
+      name: 'Triggered',
+      type: 'extra',
+      description: [
+        'You can “set” an instant duration effect with this modifier to activate under particular circumstances, such as in response to a particular danger, after a set amount of time, in response to a particular event, and so forth—chosen when you apply the modifier. Once chosen, the trigger cannot be changed.',
+        'The circumstances must be detectable by your senses. You can acquire @{Effect|Senses} @{Modifier|Limited} and @{Modifier|Linked} to Triggered effects, if desired. Setting the effect requires the same action as using it normally.',
+        'A Triggered effect lying in wait may be detected with a @{Skill|Perception} check (DC 10 + effect rank) and in some cases disarmed with a successful skill or power check (such as @{Skill|Sleight of Hand}, @{Skill|Technology}, @{Effect|Nullify} or another countering effect) with a DC of (10 + effect rank).',
+        'A Triggered effect is good for one use per rank in this modifier. After its last activation, it stops working.',
+        'You can apply an additional rank of Triggered to have a Variable Trigger, allowing you to change the effect’s trigger each time you set it.',
+      ],
+      cost: {
+        costs: [1],
+        maxRanks: -1,
+        type: 'flat',
+      },
+      source: 'HH',
+    },
+  ],
+  [
+    'Variable Descriptor',
+    {
+      name: 'Variable Descriptor',
+      type: 'extra',
+      description: [
+        'You can change the descriptors of an effect with this modifier, varying them as a free action once per round. With rank 1, you can apply any of a closely related group of descriptors, such as weather, electromagnetic, temperature, and so forth. With rank 2, you can apply any of a broad group, such as any mental, magical, or technological descriptor. The GM decides if a given descriptor is appropriate in conjunction with a particular effect and this modifier.',
+      ],
+      cost: {
+        costs: [1],
+        maxRanks: 2,
+        type: 'flat',
+      },
+      source: 'HH',
+    },
+  ],
 ]);
